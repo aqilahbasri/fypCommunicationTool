b98790170f9ca8208c1bfe677b7255b199a2bb26
aqilahbasri <49358260+aqilahbasri@users.noreply.github.com>
2020-11-19 12:40:40 +0800

    kielah update QuestionList.java

--- app/src/main/java/com/example/fypcommunicationtool/QuestionList.java
+++ app/src/main/java/com/example/fypcommunicationtool/QuestionList.java
@@ -8,18 +8,28 @@ public class QuestionList{
     String option3;
     String option4;
     int correctAnswer;
+    String desc;
 
     public QuestionList(){
 
     }
 
-    public QuestionList(String question, String option1, String option2, String option3, String option4, int correctAnswer) {
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public QuestionList(String question, String option1, String option2, String option3, String option4, int correctAnswer, String desc) {
         this.question = question;
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
         this.option4 = option4;
         this.correctAnswer = correctAnswer;
+        this.desc = desc;
     }
 
     public String getQuestion() {
