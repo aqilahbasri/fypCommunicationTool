72f603d9f01be0b123915248af56c46048c38a2f
Fatin Nabila <fatinnabilamf00@gmail.com>
2021-01-07 16:57:19 +0800

    fix nav drawer and bottom nav

--- app/src/main/java/com/example/fypcommunicationtool/MainActivity.java
+++ app/src/main/java/com/example/fypcommunicationtool/MainActivity.java
@@ -3,31 +3,46 @@ package com.example.fypcommunicationtool;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private TextView actionBarTitle;
 
     private Toolbar mToolbar;
     private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawer2;
+    Boolean doubleBackToExitPressedOnce = false;
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
@@ -43,9 +58,62 @@ public class MainActivity extends AppCompatActivity {
         currentUser = mAuth.getCurrentUser();
         RootRef = FirebaseDatabase.getInstance().getReference();
 
-        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Communication");
+//        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+//        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setTitle("Communication");
+
+        //        yvonne punya toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        actionBarTitle = findViewById(R.id.toolbar_title);
+        actionBarTitle.setText("Communication");
+        ImageButton rightMenu = findViewById(R.id.ic_right_menu);
+        rightMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+                    drawer.closeDrawer(Gravity.RIGHT);
+                } else {
+                    drawer.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+        setDefaultFragment();
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        goToMainCommunicationModule();
+                        break;
+                    case R.id.nav_learning:
+                        goToMainLearningModule();
+                        break;
+                    case R.id.nav_assessment:
+                        goToMainAssessmentModule();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.END);
+                return true;
+            }
+        });
+
+
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,
+                    new ChatsFragment()).commit();
+        }
 
         bottomNavigationView = findViewById(R.id.main_tabs);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -91,7 +159,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        setDefaultFragment();
     }
 
     private void setDefaultFragment() {
@@ -99,6 +166,76 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.main_frame, ChatsFragment.getInstance());
         transaction.commit();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            Intent intent = new Intent(this, MainActivityLearning.class);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        Fragment selectedFragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: //nanti tukar Fatin punya
+                goToMainCommunicationModule();
+//                selectedFragment = new ChatsFragment();
+                break;
+            case R.id.nav_learning:
+                goToMainLearningModule();
+//                selectedFragment = new CategoryFragment();
+                break;
+            case R.id.nav_assessment:
+                goToMainAssessmentModule();
+//                selectedFragment = new AssessmentHomeFragment();
+                break;
+//            case R.id.nav_administration:
+//                selectedFragment = new AdministrationMenu_Fragment();
+//                break;
+        }
+//
+////        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).addToBackStack(null).commit();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//
+//        if (drawer.isDrawerOpen(GravityCompat.END)) {
+//            drawer.closeDrawer(GravityCompat.END);
+//        }
+//
+//        else if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+//            getSupportFragmentManager().popBackStack();
+//        }
+//
+//        else if (isTaskRoot()) {    //Only can exit from Home fragment
+//            if (doubleBackToExitPressedOnce) {
+//                super.onBackPressed();
+//            }
+//            else {
+//                doubleBackToExitPressedOnce = true;
+//                Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show();
+//
+//                new Handler().postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        doubleBackToExitPressedOnce=false;
+//                    }
+//                }, 2000);
+//            }
+//        }
+//
+//        else {
+//            super.onBackPressed();
+//        }
+//    }
 
     @Override
     protected void onStart() {
@@ -134,42 +271,47 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mainAssessmentModule);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.options_menu, menu);
-        return true;
+    private void goToMainCommunicationModule() {
+        Intent mainCommunicationModule = new Intent(com.example.fypcommunicationtool.MainActivity.this, MainActivity.class);
+        startActivity(mainCommunicationModule);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.options_menu, menu);
+//        return true;
+//    }
 
-        if(item.getItemId()==R.id.menu_communication_module){
-
-        }
-        if(item.getItemId()==R.id.menu_add_group){
-            RequestNewGroup();
-        }
-        if(item.getItemId()==R.id.menu_find_friends){
-            SendUserToFindFriendsActivity();
-        }
-        if(item.getItemId()==R.id.menu_learning_module){
-            goToMainLearningModule();
-        }
-        if(item.getItemId()==R.id.menu_assessment_module){
-            goToMainAssessmentModule();
-        }
-        if(item.getItemId()==R.id.menu_setting){
-            SendUserToSettingActivity();
-        }
-        if(item.getItemId()==R.id.menu_signout){
-            mAuth.signOut();
-            SendUserToLoginActivity();
-        }
-
-        return true;
-    }
+// @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        super.onOptionsItemSelected(item);
+//
+//        if(item.getItemId()==R.id.menu_communication_module){
+//
+//        }
+//        if(item.getItemId()==R.id.menu_add_group){
+//            RequestNewGroup();
+//        }
+//        if(item.getItemId()==R.id.menu_find_friends){
+//            SendUserToFindFriendsActivity();
+//        }
+//        if(item.getItemId()==R.id.menu_learning_module){
+//            goToMainLearningModule();
+//        }
+//        if(item.getItemId()==R.id.menu_assessment_module){
+//            goToMainAssessmentModule();
+//        }
+//        if(item.getItemId()==R.id.menu_setting){
+//            SendUserToSettingActivity();
+//        }
+//        if(item.getItemId()==R.id.menu_signout){
+//            mAuth.signOut();
+//            SendUserToLoginActivity();
+//        }
+//
+//        return true;
+//    }
 
     private void RequestNewGroup() {
         AlertDialog.Builder builder = new AlertDialog.Builder(com.example.fypcommunicationtool.MainActivity.this, R.style.AlertDialog);
